33d1cd3b443c7e117168fb104ac444a3c7aaa3df
henry701 <henry_tuori@hotmail.com>
2018-09-23 12:55:03 -0300

    Add Retrieve Post controller (by id and by author id)

---
+++ RestServer/Controllers/RetrievePostController.cs
@@ -0,0 +1,119 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using RestServer.Model.Config;
+using RestServer.Model.Http.Response;
+using RestServer.Util;
+using RestServer.Util.Extensions;
+using System.Dynamic;
+
+namespace RestServer.Controllers
+{
+    [Route("/post/")]
+    [Controller]
+    internal sealed class RetrievePostController : ControllerBase
+    {
+        private readonly ILogger<RetrievePostController> Logger;
+        private readonly MongoWrapper MongoWrapper;
+        private readonly ServerInfo ServerInfo;
+
+        public RetrievePostController(MongoWrapper mongoWrapper, ILogger<RetrievePostController> logger)
+        {
+            Logger = logger;
+            Logger.LogTrace($"{nameof(RetrievePostController)} Constructor Invoked");
+            MongoWrapper = mongoWrapper;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<dynamic> GetById(string id)
+        {
+            var postCollection = MongoWrapper.Database.GetCollection<Post>(nameof(Post));
+
+            var postFilterBuilder = new FilterDefinitionBuilder<Post>();
+            var postFilter = postFilterBuilder.And
+            (
+                postFilterBuilder.Eq(u => u._id, new ObjectId(id)),
+                GeneralUtils.NotDeactivated(postFilterBuilder)
+            );
+
+            var post = (await postCollection.FindAsync(postFilter, new FindOptions<Post>
+            {
+                Limit = 1,
+            })).SingleOrDefault();
+
+            if (post == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return new ResponseBody
+                {
+                    Code = ResponseCode.NotFound,
+                    Success = false,
+                    Message = "Post não encontrado!",
+                };
+            }
+
+            return new ResponseBody
+            {
+                Code = ResponseCode.GenericSuccess,
+                Success = true,
+                Message = "Post encontrado com sucesso!",
+                Data = BuildPostResponse(post),
+            };
+        }
+
+        [AllowAnonymous]
+        [HttpGet("author/{id}")]
+        public async Task<dynamic> GetByAuthorId(string id)
+        {
+            var postCollection = MongoWrapper.Database.GetCollection<Post>(nameof(Post));
+
+            var postFilterBuilder = new FilterDefinitionBuilder<Post>();
+            var postFilter = postFilterBuilder.And
+            (
+                postFilterBuilder.Eq(p => p.Poster._id, new ObjectId(id)),
+                GeneralUtils.NotDeactivated(postFilterBuilder)
+            );
+
+            var posts = await postCollection.FindAsync(postFilter, new FindOptions<Post>
+            {
+                AllowPartialResults = true,
+            });
+
+            return new ResponseBody
+            {
+                Code = ResponseCode.GenericSuccess,
+                Success = true,
+                Message = "Posts encontrados com sucesso!",
+                Data = posts.ToList().Select(BuildPostResponse),
+            };
+        }
+
+        private static dynamic BuildPostResponse(Post post)
+        {
+            return new
+            {
+                Titulo = post.Title,
+                Descricao = post.Text,
+                Autor = post.Poster,
+                Likes = (int) post.Likes,
+                Midias = post.FileReferences.Select
+                    (
+                        fr => new
+                        {
+                            Id = fr._id.ToString(),
+                            TipoMidia = fr.FileMetadata.FileType.GetAttribute<DisplayAttribute>().ShortName,
+                        }
+                   )
+            };
+        }
+    }
+}
+ 
--- RestServer/Controllers/RetrieveUserController.cs
+++ RestServer/Controllers/RetrieveUserController.cs
@@ -96,12 +96,6 @@ namespace RestServer.Controllers
 
         private static void IncrementMusicianObject(Musician musician, dynamic userObj)
         {
-            //Da um erro mesmo quando o cara nao tem musicas
-            /*
-             RuntimeBinderException: '<>f__AnonymousType5<<>f__AnonymousType6<<>f__AnonymousType7<string,string,string,string>,Models.ImageReference,string,string,Models.PhoneNumber,string>>' 
-             does not contain a definition for 'musicas'
-             */
-             
             userObj.usuario.musicas = musician.Songs?.Where(s => s != null).Select(song => new
             {
                 nome = song.Name,
