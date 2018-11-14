9ce9fec56e294fd71c008aa5cfab62f88c3c0ff2
henry701 <henry_tuori@hotmail.com>
2018-11-13 22:16:44 -0200

    Start geolocation controller, commit the DLLs to the repo

---
+++ RestServer/Controllers/Other/GeolocationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using RestServer.Infrastructure.AspNetCore;
+using RestServer.Model.Config;
+using RestServer.Model.Http.Response;
+using RestServer.Util;
+using RestServer.Util.Extensions;
+
+namespace RestServer.Controllers.Other
+{
+    [Route("/geo")]
+    [Controller]
+    internal sealed class GeolocationController : ControllerBase
+    {
+        private readonly ILogger<GeolocationController> Logger;
+        private readonly MongoWrapper MongoWrapper;
+
+        public GeolocationController(MongoWrapper mongoWrapper, ILogger<GeolocationController> logger)
+        {
+            Logger = logger;
+            Logger.LogTrace($"{nameof(GeolocationController)} Constructor Invoked");
+            MongoWrapper = mongoWrapper;
+        }
+
+        [HttpGet]
+        public async Task<dynamic> Get()
+        {
+
+
+            return new ResponseBody
+            {
+                Code = ResponseCode.GenericSuccess,
+                Success = true,
+                // Message = "Geolocalização atualizada com sucesso!",
+            };
+        }
+    }
+}
--- RestServer/TODO.md
+++ RestServer/TODO.md
@@ -1,6 +1,13 @@
 ﻿# FindFm - TODO
 
 Requisito não funcional: Limite de tamanho consumido no DB por usuário (disponibilidade p/ outros e segurança)
+	- TESTAR
+
+- API Gerenciar Trabalhos
+- Recebimento de Geolocalização
+- Arrumar algoritmo do Feed
+- Arrumar algoritmo da Rádio
+- Denunciar Publicação/Mídia/Usuário
 
 Ver uma lógica de limite para anúncio, coloquei 10 só para teste da tela de Anuncios sugeridos, talvez fazer uma API para retornar apenas os anuncios?
 	- Paginação com Limit e Skip do MongoDb via QueryParameter padronizado (pode ser limit e skip mesmo e o app padroniza pra cada tipo quanto ele quer ali)
