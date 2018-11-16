98c3ad67f20cbba1453f86272705d52602bfb64d
henry701 <henry_tuori@hotmail.com>
2018-11-16 21:37:55 -0200

    Create ReportRequest.cs

---
+++ RestServer/Model/Http/Request/ReportRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace RestServer.Model.Http.Request
+{
+    [BindRequired]
+    internal sealed class ReportRequest
+    {
+        [Required]
+        public string Id;
+        [Required]
+        public TipoDenuncia Tipo;
+        [Required]
+        public string Motivo;
+        [Required]
+        public string Contato;
+    }
+
+    internal enum TipoDenuncia
+    {
+        Post,
+        Perfil,
+        Anúncio,
+        Música,
+        Trabalho,
+        Imagem,
+        Vídeo,
+        Comentário
+    }
+}
